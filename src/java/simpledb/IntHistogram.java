a4e6a14f06444f5de4c3674faeafa6af8c516197
Surface Laptop 3 <rui@Surface-Laptop-3.local>
2022-01-19 16:00:21 +0800

    pass IntHistogramTest

--- src/java/simpledb/IntHistogram.java
+++ src/java/simpledb/IntHistogram.java
@@ -1,8 +1,60 @@
 package simpledb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /** A class to represent a fixed-width histogram over a single integer-based field.
  */
 public class IntHistogram {
+
+    private class Bucket {
+        private int left;
+        private int right;
+        private int cnt;
+
+        public Bucket(int left, int right) {
+            this.left = left;
+            this.right = right;
+            cnt = 0;
+        }
+
+        public int getLeft() {
+            return left;
+        }
+
+        public int getRight() {
+            return right;
+        }
+
+        public int getCnt() {
+            return cnt;
+        }
+
+        public void setCnt(int cnt) {
+            this.cnt = cnt;
+        }
+
+        public int getWidth() {
+            // don't forget to plus one
+            return right - left + 1;
+        }
+
+        public void increase() {
+            cnt++;
+        }
+
+        @Override
+        public String toString() {
+            return "<" + left + "," + right + ">:" + cnt;
+        }
+    }
+
+    private int numBuckets;
+    private int minValue;
+    private int maxValue;
+    private double width;
+    private List<Bucket> buckets;
+    private int nTup;
 
     /**
      * Create a new IntHistogram.
@@ -21,7 +73,26 @@ public class IntHistogram {
      * @param max The maximum integer value that will ever be passed to this class for histogramming
      */
     public IntHistogram(int buckets, int min, int max) {
-    	// some code goes here
+    	// Done
+        numBuckets = buckets;
+        minValue = min;
+        maxValue = max;
+        this.buckets = new ArrayList<>();
+        // don't forget to plus one
+        this.width = (1.0 + max - min) / buckets;
+        for (int i = 0; i < numBuckets; i++) {
+            int left = (int) Math.ceil(min + i * width);
+            int right = (int) Math.ceil(min + (i + 1) * width) - 1;
+            if (right < left) {
+                right = left;
+            }
+
+            this.buckets.add(new Bucket(left, right));
+        }
+    }
+
+    private int getBucketIndex(int v) {
+        return (int) ((v - minValue) / width);
     }
 
     /**
@@ -29,7 +100,13 @@ public class IntHistogram {
      * @param v Value to add to the histogram
      */
     public void addValue(int v) {
-    	// some code goes here
+    	// Done
+        if (v < minValue || v > maxValue) {
+            throw new IllegalArgumentException("Value out of range");
+        }
+        int idx = getBucketIndex(v);
+        buckets.get(idx).increase();
+        nTup++;
     }
 
     /**
@@ -43,9 +120,57 @@ public class IntHistogram {
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
+    	// Done
+        int index;
+        double sum;
+        Bucket bucket;
 
-    	// some code goes here
-        return -1.0;
+        switch (op) {
+            case EQUALS:
+                index = getBucketIndex(v);
+                if (index < 0 || index >= numBuckets) {
+                    return 0.0;
+                }
+                bucket = buckets.get(index);
+                // (h / w) / nTup
+                return (1.0 * bucket.getCnt() / bucket.getWidth()) / nTup;
+            case GREATER_THAN:
+                index = getBucketIndex(v);
+                if (index < 0) {
+                    return 1.0;
+                }
+                if (index >= numBuckets) {
+                    return 0.0;
+                }
+                bucket = buckets.get(index);
+                sum = 1.0 * bucket.getCnt() * (bucket.getRight() - v) / bucket.getWidth();
+                for (int i = index + 1; i < numBuckets; i++) {
+                    sum += buckets.get(i).getCnt();
+                }
+                return sum / nTup;
+            case LESS_THAN:
+                index = getBucketIndex(v);
+                if (index < 0) {
+                    return 0.0;
+                }
+                if (index >= numBuckets) {
+                    return 1.0;
+                }
+                bucket = buckets.get(index);
+                sum = 1.0 * bucket.getCnt() * (v - bucket.getLeft()) / bucket.getWidth();
+                for (int i = index - 1; i >= 0; i--) {
+                    sum += buckets.get(i).getCnt();
+                }
+                return sum / nTup;
+            case GREATER_THAN_OR_EQ:
+                return estimateSelectivity(Predicate.Op.GREATER_THAN, v - 1);
+            case NOT_EQUALS:
+                return 1 - estimateSelectivity(Predicate.Op.EQUALS, v);
+            case LESS_THAN_OR_EQ:
+                return estimateSelectivity(Predicate.Op.LESS_THAN, v + 1);
+            default:
+                throw new UnsupportedOperationException();
+        }
     }
     
     /**
@@ -58,15 +183,24 @@ public class IntHistogram {
      * */
     public double avgSelectivity()
     {
-        // some code goes here
-        return 1.0;
+        // Done
+        int cnt = 0;
+        for (Bucket b : buckets) {
+            cnt += b.getCnt();
+        }
+        return 1.0 * cnt / numBuckets;
     }
     
     /**
      * @return A string describing this histogram, for debugging purposes
      */
     public String toString() {
-        // some code goes here
-        return null;
+        // Done
+        StringBuilder res = new StringBuilder("|| ");
+        for (Bucket b : buckets) {
+            res.append(b);
+            res.append(" || ");
+        }
+        return res.toString();
     }
 }
